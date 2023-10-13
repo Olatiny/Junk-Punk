dd99995f89009bfbc613f7166879d1a115f7ea00
Olatiny <thomascgelman@gmail.com>
2023-10-13 15:08:00 -0400

    Initial Commit with test code

---
+++ Scripts/2DMovement.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+var speedVec = Vector2.ZERO
+var direction = Vector2.ZERO
+
+var tileWidth = 16
+var tileHeight = 13
+
+func _ready():
+	speedVec.x = tileWidth
+	speedVec.y = tileHeight
+
+func get_input():
+	direction = Input.get_vector("Left", "Right", "Up", "Down")
+
+func _unhandled_input(event):
+	direction = Input.get_vector("Left", "Right", "Up", "Down")
+	move(direction)
+	
+func move(dir):
+	if (dir.x != 0 && dir.y != 0):
+		position += dir * speedVec * sqrt(2)
+	else:
+		position += dir * speedVec
---
+++ Scripts/ChessBoard.gd
@@ -0,0 +1,20 @@
+extends TileMap
+
+var grid = []
+var gridWidth = 8
+var gridHeight = 8
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in gridWidth:
+		grid.append([])
+		for j in gridHeight:
+			grid[i].append(null)
+	pass # Replace with function body.
+
+func RequestMove(obj, dir):
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
